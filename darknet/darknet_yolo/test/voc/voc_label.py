054e220df09580d66565f39f998e6faaf824e070
good74152 <39672039+good74152@users.noreply.github.com>
2019-05-24 09:56:45 +0800

    Update voc_label.py

--- darknet/darknet_yolo/test/voc/voc_label.py
+++ darknet/darknet_yolo/test/voc/voc_label.py
@@ -6,7 +6,7 @@ from os.path import join
 
 sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
 
-classes = ["helmet", "mask", "phone"]
+classes = ["helmet", "mask", "phone", "talk"]
 
 
 def convert(size, box):
